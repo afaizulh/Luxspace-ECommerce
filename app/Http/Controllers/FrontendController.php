90baff73d6f2654714657df0b63f40b8e1d4e7af
afaizulh <ijulhakim14@gmail.com>
2023-07-03 20:55:41 +0700

    edit

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function index(Request $request)
     {
-        return view('page.frontend.index');
+        $products = Products::with(['gallery'])->latest()->get();
+
+        return view('page.frontend.index', compact('products'));
     }
 
     public function details(Request $request, $slug)
